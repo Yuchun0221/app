f5f5f559c22232b9a920092e825bd71808ac2f2e
Yuchun0221 <hyc1327@gmail.com>
2022-11-30 13:46:42 +0800

    main3

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val ed_peoplenum = findViewById<EditText>(R.id.ed_peoplenum)
+        val btn_commit1 = findViewById<Button>(R.id.btn_commit1)
+        val ed_moneynum = findViewById<EditText>(R.id.ed_moneynum)
     }
 }
