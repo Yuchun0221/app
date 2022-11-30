d818d31941cf75a5532cb0c8e485b61f2ec59491
Yuchun0221 <hyc1327@gmail.com>
2022-11-30 16:49:17 +0800

    3

--- app/src/main/java/com/example/myapplication/MainActivity4.kt
+++ app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,7 @@ class MainActivity4 : AppCompatActivity() {
         setContentView(R.layout.activity_main4)
 
         val ed_itemName = findViewById<EditText>(R.id.ed_itemName)
+        val ed_pay = findViewById<EditText>(R.id.ed_pay)
+        val textView3 = findViewById<TextView>(R.id.textView3)
     }
 }
