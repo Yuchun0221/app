8d0be46429f1d1ad5977508ee5e5199d74956905
Yuchun0221 <hyc1327@gmail.com>
2022-11-30 13:01:39 +0800

    main1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView１ = findViewById<TextView>(R.id.textView１)
+        val btn_typeone = findViewById<Button>(R.id.btn_typeone)
+        val btn_typetwo = findViewById<Button>(R.id.btn_typetwo)
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
 
 class MainActivity2 : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val btn_commit = findViewById<Button>(R.id.btn_commit)
+
     }
 }
